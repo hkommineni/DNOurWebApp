1e281c7bac7c4db072ffbb8c4cf5ac08c7aeef1c
Chris Behrens <cbehrens@gmail.com>
2018-07-28 12:17:22 -0500

    Added config stuff

--- OurWebApp.Web/Pages/Index.cshtml.cs
+++ OurWebApp.Web/Pages/Index.cshtml.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace OurWebApp.Web.Pages
+﻿namespace OurWebApp.Web.Pages
 {
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Configuration;
+
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration Config;
+
+        // Constructor that that takes IConfiguration is called on instantiation thanks to Dependency injection
+        public IndexModel(IConfiguration config)
+        {
+            Config = config;
+        }
+
         public void OnGet()
         {
 
--- OurWebApp.Web/Startup.cs
+++ OurWebApp.Web/Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace OurWebApp.Web
 {
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -16,17 +12,26 @@ namespace OurWebApp.Web
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSingleton(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
             if (env.IsDevelopment())
             {
                 app.UseBrowserLink();
